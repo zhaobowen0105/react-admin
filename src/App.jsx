2aa0efd85bf1925c1f0af1e09725b17526c3f575
zhaobowen <957328000@qq.com>
2019-05-14 11:24:32 +0800

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import {Button, message} from 'antd'
+import {Button, message} from 'antd';
 
 export default class App extends Component{
   handleClick = () => {
     message.success('成功啦...');
-  }
+  };
 
   render() {
-    return <Button type="primary" onClick={this.handleClick}>测试button</Button>
+    return <Button type="primary" onClick={this.handleClick}>测试button</Button>;
   }
 }
