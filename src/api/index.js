f2983e9b2737dfaaf34d3655b8b8449e3622d0fc
zhaobowen <957328000@qq.com>
2019-05-17 09:52:47 +0800

    header完成

--- src/api/index.js
+++ src/api/index.js
@@ -22,3 +22,27 @@ export const reqWeather = city => {
     })
   })
 };
+
+// 添加角色
+export const reqAddRole = (roleName) => ajax('/manage/role/add', {roleName}, 'POST');
+
+// 获取角色列表
+export const reqRoles = () => ajax('/manage/role/list');
+
+// 更新角色(给角色设置权限)
+export const reqUpdateRole = (role) => ajax('/manage/role/update', role, 'POST');
+
+// 获取一级或某个二级分类列表
+export const reqCategorys = (parentId = '0') => ajax('/manage/category/list', {parentId});
+
+// 添加分类
+export const reqAddCategory = (parentId, categoryName) => ajax('/manage/category/add', {
+  parentId,
+  categoryName
+}, 'POST');
+
+// 更新品类名称
+export const reqUpdateCategory = ({categoryId, categoryName}) => ajax('/manage/category/update', {
+  categoryId,
+  categoryName
+}, 'POST');
