0ebdcec68c24df533c5c57a20adb101fe5b38d7a
zhaobowen <957328000@qq.com>
2019-05-25 11:47:38 +0800

    echarts和bizcharts

--- src/pages/charts/line.jsx
+++ src/pages/charts/line.jsx
@@ -1,10 +1,61 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
+import {Card, Button} from 'antd'
+import ReactEcharts from 'echarts-for-react'
 
-export default class Line extends Component{
+/*
+ 后台管理的折线图路由组件
+ */
+export default class Bar extends Component {
+  state= {
+    sales: [5, 20, 36, 10, 10, 20],
+    stores: [6, 8, 25, 15, 13, 22]
+  }
+
+  update = () => {
+    this.setState(state => ({
+      sales: state.sales.map(sale => sale + 1),
+      stores: state.stores.map(store => store - 1)
+    }))
+  }
+
+  getOption = (sales, stores) => {
+    return {
+      title: {
+        text: 'ECharts 入门示例'
+      },
+      tooltip: {},
+      legend: {
+        data:['销量', '库存']
+      },
+      xAxis: {
+        data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+      },
+      yAxis: {},
+      series: [{
+        name: '销量',
+        type: 'line',
+        data: sales
+      },{
+        name: '库存',
+        type: 'line',
+        data: stores
+      }]
+    }
+  }
 
   render() {
-    return <div>
-      Line
-    </div>
+    const { sales, stores } = this.state
+    return (
+      <div>
+        <Card>
+          <Button type='primary' onClick={this.update}>更新</Button>
+        </Card>
+
+        <Card title='折线图一'>
+          <ReactEcharts option={this.getOption(sales, stores)} style={{height: 300}}/>
+        </Card>
+
+      </div>
+    )
   }
 }
